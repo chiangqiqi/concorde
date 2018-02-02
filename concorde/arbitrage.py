d77c770b6c5ab4a2007b55b8e9a3e7b84961d36e
jiangqiqi <chiangqiqi@gmail.com>
2018-02-02 10:37:25 +0800

    add argument parser

--- concorde/arbitrage.py
+++ concorde/arbitrage.py
@@ -1,21 +1,19 @@
 import logging
 import pandas as pd
 from collections import deque
+import time
+import sys
+import numpy as np
 import math
 
-"""
-polo.returnTicker
-"""
+from itertools import takewhile
+import functools
 
 # wrapper for client code
 top_price = lambda l: float(l[0][0])
 price_diff = lambda r,x,y: (x-y)/x> r
 
 parsedepth = lambda x: float(x)
-
-
-from itertools import takewhile
-import functools
 
 def amount_and_price(bask, abid, ratio):
     """return buy price and sell price for two exchange depth array, and the amount
@@ -49,26 +47,31 @@ def precision_floor(f, presicion=2):
 
 
 class Arbitrager:
-    def __init__(self, exchangeA, exchangeB, ratio=0.0025, precision=3, informer=None):
+    def __init__(self, exchangeA, exchangeB,
+                 ratio=0.0025, precision=3, informer=None, use_avg=False, max_amount=None):
         """
         precision: Binance sometimes got a LOT error if the amount is with a presicion higher
-        """ 
+        """
         self.exchangeA = exchangeA
         self.exchangeB = exchangeB
         # self._coinA = coinA
         # self._coinB = coinB
         self.threshold=0.0001
         self.ratio = ratio
-        self.use_avg=False
+        self.use_avg = use_avg
         self.amt_precision = precision
         self.informer = informer
+        # max buy in amount
+        self.max_amount = max_amount
+        # 2 hour MA
+        self.q = deque(maxlen=7200)
 
     def set_config(self, config):
         """
         """
         self.threshold = float(config['threshold'])
         self.amt_precision = int(config['precision'])
-        
+
     def run(self, coinA, coinB):
         """keep in mind  ask price is higher than
         coinB: 基准货币，比如 btc, usdt
@@ -85,24 +88,24 @@ class Arbitrager:
         p_bid, b_bid = top_price(huobi_price['bids']), top_price(bina_price['bids'])
 
         if self.use_avg:
-            q.append((p_ask, b_ask, p_bid, b_bid))
-            arr = np.array(q)
+            self.q.append((p_ask, b_ask, p_bid, b_bid))
+            arr = np.array(self.q)
             avg_price(arr)
 
             huobi_div_bina = avg_price(arr)
             logging.info("ratio is {}".format(huobi_div_bina))
 
             # deform the other platform price
-            bina_price = {}
-            bina_price['asks'] = price_deform(bina_price['asks'], 1/huobi_div_bina)
-            bina_price['bids'] = price_deform(bina_price['bids'], 1/huobi_div_bina)
+            huobi_price = {}
+            huobi_price['asks'] = price_deform(bina_price['asks'], huobi_div_bina)
+            huobi_price['bids'] = price_deform(bina_price['bids'], huobi_div_bina)
 
             # 买一价和卖一价, bid is higher than asks
             p_ask, b_ask = top_price(huobi_price['asks']), top_price(bina_price['asks'])
             p_bid, b_bid = top_price(huobi_price['bids']), top_price(bina_price['bids'])
 
-            if len(q) < 100:
-                logging.info("ticker length is {}".format(len(q)))
+            if len(self.q) < 100:
+                logging.info("ticker length is {}".format(len(self.q)))
                 return
 
         logging.info("binance price is {}, {}".format(b_ask, b_bid))
@@ -117,11 +120,16 @@ class Arbitrager:
                                                             format_ticker(bina_price['bids']), self.ratio)
 
             b_eth_amt = float(self.exchangeA.balance(coinA))
+            b_usdt_amt = float(self.exchangeA.balance(coinB))
             # p_usdt_amt = float(self.exchangeA.balance(coinB))
 
             amt = b_eth_amt
             # amt = min(amt, p_usdt_amt/p_buy_price)
             # limit precision
+
+            if self.max_amount is not None:
+                amt = min(amt, (self.max_amount - b_usdt_amt)/b_sell_price)
+
             amt = precision_floor(amt, self.amt_precision)
 
             if amt> self.threshold:
@@ -145,6 +153,7 @@ class Arbitrager:
             # cut by a little margin to avoid lot error
             amt = min(amt, b_usdt_amt/b_buy_price)
             amt = precision_floor(amt, self.amt_precision)
+
             if amt> self.threshold:
                 # print("buy")
                 self.exchangeA.trade(bina_str, b_buy_price, amt, "Buy")
@@ -156,11 +165,6 @@ class Arbitrager:
                 logging.info("not enogh usdt {} to trade".format(b_usdt_amt))
                 # logging.info("not enogh {} {} to trade".format(coinA ,p_eth_amt))
 
-import time
-import sys
-import numpy as np
-
-q = deque(maxlen=3600)
 
 def avg_price(arr):
     """average price of usdt between two platform
