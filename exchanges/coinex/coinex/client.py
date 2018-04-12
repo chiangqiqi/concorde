9e17b318af01b216eb30e50b8880dd96f26c3363
jiangqiqi <chiangqiqi@gmail.com>
2018-04-12 08:24:15 +0800

    change market maker

--- exchanges/coinex/coinex/client.py
+++ exchanges/coinex/coinex/client.py
@@ -9,6 +9,7 @@ import json as complex_json
 import requests
 import logging
 
+
 class RequestClient(object):
     __headers = {
         'Content-Type': 'application/json; charset=utf-8',
@@ -42,21 +43,30 @@ class RequestClient(object):
         if method == 'GET':
             self.set_authorization(params)
             return requests.get(url, params=params, headers=self.headers)
+        if method == 'DELETE':
+            self.set_authorization(params)
+            return requests.delete(url, params=params, headers=self.headers)
         else:
             if data:
                 json.update(complex_json.loads(data))
             self.set_authorization(json)
             return requests.request(method, url, json=json, headers=self.headers)
 
+# from datetime import datetime
+import time       
+
 class Client:
     def __init__(self, pkey, skey):
         self.auth = RequestClient(pkey, skey)
+        self._pkey = pkey
         
     def get_account(self):
         resp = self.auth.request('GET', 'https://api.coinex.com/v1/balance/')
         return resp.json()
 
     def trade(self, market, price, amt, ttype):
+        price = '%.7f' % (price)
+
         data = {
             "amount": amt,
             "price": price,
@@ -71,7 +81,44 @@ class Client:
         )
         logging.info('place {} order in market {} at price {} with amt {}'.format(ttype, market, price, amt))
         return resp.json()
+    
+    def get_orders(self, market):
+        """
+        Get current orders
+        """ 
+        tonce = int(time.time() * 1000)
+        data = {
+            'market' : market, 
+            'access_id': self._pkey,
+            'page': 1,
+            'limit': 100,
+            'tonce': tonce
+        }
+        resp = self.auth.request(
+            'GET', 
+            'https://api.coinex.com/v1/order/pending',
+            params = data
+        )
+        return resp
 
+    def delete_order(self, order_id, market):
+        """Cancel an order 
+        """ 
+        tonce = int(time.time() * 1000)
+        data = {
+            'access_id': self._pkey,
+            'id': order_id, 
+            'market': market,
+            'tonce': tonce
+        }
+        resp = self.auth.request(
+            'DELETE',
+            'https://api.coinex.com/v1/order/pending',
+            params=data
+        )
+        return resp.json()
+        
+    
     def depth(self, market):
         resp = self.auth.request(
             'GET',
@@ -79,3 +126,7 @@ class Client:
             params={'market': market, 'merge': 0}
         )
         return resp.json()
+
+    def cancel_all_orders(self, market):
+        resp = self.get_orders(market)
+        return [self.delete_order(d['id'], 'LTCBTC') for d in resp.json()['data']['data']]
--- exchanges/coinex/marketmaker.py
+++ exchanges/coinex/marketmaker.py
@@ -1,17 +1,39 @@
 #!/usr/bin/env python
 
-
-pkey = '***'
-skey = '***'
-
 from coinex.client import Client
 
+pkey = '5316330BEF5F43D3B656D9760F95202C'
+skey = '02C9A322D79E466FAFACB47D400C2301E50A0F70D94BE826'
+
+from pymongo import MongoClient
+
+mc = MongoClient()
+orders = mc['coinex']['orders']
 client = Client(pkey, skey)
 
 
-account =  client.get_account()
+# account =  client.get_account()
 
 top_price = lambda l: float(l[0][0])
+
+from collections import namedtuple
+
+placed_orders = []
+
+Order = namedtuple("Order", ['type', 'price', 'amount'])
+
+
+def refresh_balance():
+    resp = client.get_account()
+    d = {}
+    data = resp['data']
+    
+    for coin in data:
+        d[coin] = float(data[coin]['available'])
+    
+    balance = d
+    return d
+
 
 def mmake(market, ratio=0.001, amt=0.1):
     depth = client.depth(market)['data']
@@ -19,13 +41,58 @@ def mmake(market, ratio=0.001, amt=0.1):
     top_bid,top_ask = top_price(bids),top_price(asks)
     
     mid = (top_ask + top_bid)/2
-    if top_ask/top_bid > 1.002:
+    
+    balance = refresh_balance()
+   
+    if top_ask/top_bid < 1.002:
+        print("no price gap")
+    else:
         ask_price = mid * (1+ratio)
         bid_price = mid * (1-ratio)
         
-        import pdb; pdb.set_trace()
+        # check if we have enogh balance
+        if balance['BTC'] < amt * ask_price:
+            print("no enogh btc {}".format(balance))
+            return 
+
+       
+        # import pdb; pdb.set_trace()
         # place 2 orders
-        client.trade(market, ask_price, amt, 'sell')
-        client.trade(market, bid_price, amt, 'buy')
+        resp = client.trade(market, ask_price, amt, 'sell')
+        print("placing sell order")
+        print(resp)
+        orders.insert(resp)
+        placed_orders.append(Order('sell', ask_price, amt))
+
+        resp = client.trade(market, bid_price, amt, 'buy')
+        print("placing sell order")
+        print(resp)
+        placed_orders.append(Order('buy', bid_price, amt))
+        orders.insert(resp)
+        
+def account_balance(coina, coinb):
+    if coina == 'BTC':
+        market = coinb+coina
+    else:
+        market = coina+coinb
+   
+    balance = refresh_balance()
     
-mmake('ltcbtc')
+    depth = client.depth(market)['data']
+    asks,bids = depth['asks'], depth['bids']
+    top_bid,top_ask = top_price(bids),top_price(asks)
+    
+    # coinb is btc too much ltc, too little btc
+    if balance[coinb] < 1/3 * top_bid * balance[coina]:
+        client.trade(market, top_ask * (1-0.0005), 0.1, 'sell')
+    elif top_bid * balance[coina] < 1/3 * balance[coinb]:
+        client.trade(market, top_bid * (1+0.0005), 0.1, 'buy')
+    
+        
+import time
+# mmake('ltcbtc')
+if __name__ == '__main__':
+    while 1:
+        account_balance('LTC', 'BTC')
+        mmake('ltcbtc')
+        time.sleep(1)
